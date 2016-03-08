e25d8a504ebac35d6dcaa2e175a44cbfde9a6841
Justin Mancherje <mancherje.justin@gmail.com>
2016-03-07 19:50:18 -0800

    update app.js in components

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, Sweet.</h1>
+        <h3>This is ok</h3>
+      </div>
+    );
+  }
+}
